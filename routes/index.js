e4cec6b59d1d9691deb7ab17780a7a5b34b9fa90
damoonguy <mmoonan@stevens.edu>
2024-12-02 14:39:08 -0500

    re-structure

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 //Here you will require route files and export them as used in previous labs.
 //Here you will import route files and export them as used in previous labs
 
-
+import { Router } from "express";
 
 const buildRoutes = (app) => {
     app.use('/admin')
@@ -10,5 +10,15 @@ const buildRoutes = (app) => {
         return res.status(404).json({error: 'Not Found'})
     });
 };
+
+const router = Router();
+
+router.route('/login').get(async (req, res) => {
+    res.render('login.handlebars')
+})
+
+router.route('/register').get(async (req, res) => {
+    res.render('register.handlebars')
+})
 
 export default buildRoutes;
