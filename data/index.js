436681dac0bd3a41c4e0b33c36479672710a973d
salvare3 <salvare3@stevens.edu>
2024-12-08 20:10:04 -0500

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,9 @@
+import userDataFunctions from './users.js';
+import questionDataFunctions from './questions.js';
+import quizDataFunctions from './quizzes.js';
+import subjectDataFunctions from './subjects.js';
+
+export const userData = userDataFunctions;
+export const questionData = questionDataFunctions;
+export const quizData = quizDataFunctions;
+export const subjectData = subjectDataFunctions;
