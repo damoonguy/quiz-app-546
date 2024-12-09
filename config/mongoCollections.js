51e62ce0e4dd36d0c8af092abaa650274672425a
salvare3 <salvare3@stevens.edu>
2024-12-08 20:10:08 -0500

    Update mongoCollections.js

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -14,7 +14,9 @@ const getCollectionFn = (collection) => {
 };
 
 // Just import these collections and call them on-demand to access the mongodb
-export const users = getCollectionFn('users');
-export const quizzes = getCollectionFn('quizzes'); 
+export const users = getCollectionFn('users'); // collection of users of the web app
+export const subjects = getCollectionFn('subjects'); // collection of subjects (ex. history, science, etc.)
+export const questions = getCollectionFn('questions'); // collection of the questions 
+export const quizzes = getCollectionFn('quizzes'); // collection of the quizzes
 
 export const dashboardStats = getCollectionFn('dashboardStats'); // Added this for Dashboard
