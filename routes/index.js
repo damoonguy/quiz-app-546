1cf537187136ea7138157e50d5bcf844e7a63299
kaivillamor <145311612+kaivillamor@users.noreply.github.com>
2024-12-13 22:04:34 -0500

    added import for profileRoutes

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,22 @@
 import userRoutes from "./users.js"
 import adminRoutes from "./admin.js"
 import quizRoutes from "./quizzes.js"
-import { dashboardRoutes } from "./dashboardRoutes.js"
-
+import profileRoutes from "./profile.js" // got rid of dashboard routes will work on after
 
 const buildRoutes = (app) => {
     app.use('/admin', adminRoutes)
-    app.use('/', userRoutes)
+    app.use('/users', userRoutes)
     app.use('/quiz', quizRoutes)
-    dashboardRoutes(app)
+    app.use('/profile', profileRoutes);
+    app.get('/', (req, res) => {
+        res.render('home', {
+            title: 'Home'
+        });
+    });
+    
     app.use('*', (req, res) => {
         return res.status(404).json({error: 'Not Found'})
     });
 };
-
 
 export default buildRoutes;
